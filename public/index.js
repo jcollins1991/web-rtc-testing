3f56df639eb755d0164f4d39facc3fa10043006e
Jonathan Collins <jcollins1991@gmail.com>
2022-08-03 17:30:33 -0400

    wip

---
+++ public/index.js
@@ -0,0 +1,118 @@
+
+const socket = io('http://localhost:3000');
+
+let selectedUser;
+
+const onUpdateUserList = ({ userIds }) => {
+  const usersList = document.querySelector('#usersList');
+  const usersToDisplay = userIds.filter(id => id !== socket.id);
+  usersList.innerHTML = '';
+
+  usersToDisplay.forEach(user => {
+    const userItem = document.createElement('div');
+    userItem.innerHTML = user;
+    userItem.className = 'user-item';
+
+    userItem.addEventListener('click', () => {
+      selectedUser = user;
+    });
+    usersList.appendChild(userItem);
+  });
+};
+
+socket.on('update-user-list', onUpdateUserList);
+
+const createPeerConnection = () => {
+  return new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: "stun:stun.stunprotocol.org"
+      }
+    ]
+  });
+};
+
+const peer = createPeerConnection();
+
+const onSocketConnect = async () => {
+  document.querySelector('#userId').innerHTML = `My user id is ${socket.id}`
+
+  const constraints = {
+    audio: true,
+    video: true
+  };
+
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  document.querySelector('#localVideo').srcObject = stream;
+  stream.getTracks().forEach(track => peer.addTrack(track, stream));
+
+  callButton.disabled = false;
+};
+
+socket.on('connect', onSocketConnect);
+
+const call = async () => {
+  callButton.disabled = true;
+
+  const localPeerOffer = await peer.createOffer();
+  await peer.setLocalDescription(new RTCSessionDescription(localPeerOffer));
+  socket.emit('mediaOffer', {
+    offer: localPeerOffer,
+    from: socket.id,
+    to: selectedUser
+  })
+};
+
+const callButton = document.querySelector('#call');
+callButton.addEventListener('click', call);
+
+const onMediaOffer = async (data) => {
+  try {
+    await peer.setRemoteDescription(new RTCSessionDescription(data.offer));
+    const peerAnswer = await peer.createAnswer();
+    await peer.setLocalDescription(new RTCSessionDescription(peerAnswer));
+
+    socket.emit('mediaAnswer', {
+      answer: peerAnswer,
+      from: socket.id,
+      to: data.from
+    });
+  } catch (error) {
+    // Handle error
+  }
+};
+
+socket.on('mediaOffer', onMediaOffer);
+
+const onMediaAnswer = async data => {
+  await peer.setRemoteDescription(new RTCSessionDescription(data.answer));
+};
+
+socket.on('mediaAnswer', onMediaAnswer);
+
+const onIceCandidateEvent = event => {
+  socket.emit('iceCandidate', {
+    to: selectedUser,
+    candidate: event.candidate
+  });
+};
+
+peer.onicecandidate = onIceCandidateEvent;
+
+const onRemotePeerIceCandidate = async data => {
+  try {
+    const candidate = new RTCIceCandidate(data.candidate);
+    await peer.addIceCandidate(candidate);
+  } catch (error) {
+    // Handle error
+  }
+};
+
+socket.on('remotePeerIceCandidate', onRemotePeerIceCandidate)
+
+const gotRemoteStream = event => {
+  const [stream] = event.streams;
+  document.querySelector('#remoteVideo').srcObject = stream;
+};
+
+peer.addEventListener('track', gotRemoteStream);
---
+++ server1.js
@@ -0,0 +1,55 @@
+// 1
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+http.listen(3000, () => {
+  console.log('listening on http://localhost:3000');
+});
+
+
+//2
+let connectedUsers = [];
+
+io.on('connection', socket => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  })
+
+  connectedUsers.push(socket.id);
+  socket.broadcast.emit('update-user-list', { userIds: connectedUsers });
+
+  socket.on('disconnect', () => {
+    connectedUsers = connectedUsers.filter(user => user !== socket.id);
+    socket.broadcast.emit('update-user-list', { userIds: connectedUsers });
+  });
+
+  // 4
+  socket.on('mediaOffer', data => {
+    socket.to(data.to).emit('mediaOffer', {
+      from: data.from,
+      offer: data.offer
+    });
+  });
+
+  socket.on('mediaAnswer', data => {
+    socket.to(data.to).emit('mediaAnswer', {
+      from: data.from,
+      answer: data.answer
+    });
+  });
+
+  socket.on('iceCandidate', data => {
+    socket.to(data.to).emit('remotePeerIceCandidate', {
+      candidate: data.candidate
+    });
+  });
+
+});
+
+// 3
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
